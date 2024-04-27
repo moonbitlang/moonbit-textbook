51dd4690fbc8089301e95d57964700d6462a7625
skylee03 <1178715749@qq.com>
2024-04-27 16:25:31 +0800

    fix: fix broken links

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -90,7 +90,7 @@ const config: Config = {
           items: [
             {
               label: 'Tutorial',
-              to: '/docs/intro',
+              to: '/docs/cover',
             },
           ],
         },
