6321003e17f3379172bf4558ffb878c78f994dd5
Yang Ming-Tian <1178715749@qq.com>
2024-04-28 17:31:46 +0800

    feat: remove blog sidebar and "edit this page" links (#6)
    * feat: remove blog sidebar
    
    * feat: remove "edit this page" links

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -55,17 +55,10 @@ const config: Config = {
           sidebarPath: './sidebars.ts',
           remarkPlugins: [remarkMath],
           rehypePlugins: [rehypeMathjax, rehypeShikiPlugin],
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: {
           showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          blogSidebarCount: 0,
         },
         theme: {
           customCss: './src/css/custom.css',
